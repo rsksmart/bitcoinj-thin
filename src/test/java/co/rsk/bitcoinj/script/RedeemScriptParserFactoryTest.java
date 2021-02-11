3645a713ee022e004ab8ff3ee7a661260c5cd513
Guido Medina <guido@iovlabs.org>
2021-02-11 16:02:30 -0300

    Add new methods to RedeemScriptUtils and tests

--- src/test/java/co/rsk/bitcoinj/script/RedeemScriptParserFactoryTest.java
+++ src/test/java/co/rsk/bitcoinj/script/RedeemScriptParserFactoryTest.java
@@ -13,16 +13,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class RedeemScriptParserFactoryTest {
-    private final List<BtcECKey> btcECKeyList = new ArrayList<>();
+    private final List<BtcECKey> defaultFedBtcECKeyList = new ArrayList<>();
+    private final List<BtcECKey> erpFedBtcECKeyList = new ArrayList<>();
     private final BtcECKey ecKey1 = BtcECKey.fromPrivate(BigInteger.valueOf(100));
     private final BtcECKey ecKey2 = BtcECKey.fromPrivate(BigInteger.valueOf(200));
     private final BtcECKey ecKey3 = BtcECKey.fromPrivate(BigInteger.valueOf(300));
+    private final BtcECKey ecKey4 = BtcECKey.fromPrivate(BigInteger.valueOf(400));
+    private final BtcECKey ecKey5 = BtcECKey.fromPrivate(BigInteger.valueOf(500));
+    private final BtcECKey ecKey6 = BtcECKey.fromPrivate(BigInteger.valueOf(600));
 
     @Before
     public void setUp() {
-        btcECKeyList.add(ecKey1);
-        btcECKeyList.add(ecKey2);
-        btcECKeyList.add(ecKey3);
+        defaultFedBtcECKeyList.add(ecKey1);
+        defaultFedBtcECKeyList.add(ecKey2);
+        defaultFedBtcECKeyList.add(ecKey3);
+        erpFedBtcECKeyList.add(ecKey4);
+        erpFedBtcECKeyList.add(ecKey5);
+        erpFedBtcECKeyList.add(ecKey6);
     }
 
     @Test
@@ -30,7 +37,7 @@ public class RedeemScriptParserFactoryTest {
         byte[] data = Sha256Hash.of(new byte[]{1}).getBytes();
         Script fastBridgeRedeemScript = RedeemScriptUtils.createFastBridgeRedeemScript(
             data,
-            btcECKeyList
+            defaultFedBtcECKeyList
         );
 
         RedeemScriptParser parser = RedeemScriptParserFactory.get(fastBridgeRedeemScript.getChunks());
@@ -40,10 +47,39 @@ public class RedeemScriptParserFactoryTest {
 
     @Test
     public void create_RedeemScriptParser_object_from_standard_multiSig_chunk() {
-        Script redeemScript = RedeemScriptUtils.createStandardRedeemScript(btcECKeyList);
+        Script redeemScript = RedeemScriptUtils.createStandardRedeemScript(defaultFedBtcECKeyList);
         RedeemScriptParser parser = RedeemScriptParserFactory.get(redeemScript.getChunks());
 
         Assert.assertEquals(MultiSigType.STANDARD_MULTISIG, parser.getMultiSigType());
+        Assert.assertEquals(ScriptType.REDEEM_SCRIPT, parser.getScriptType());
+    }
+
+    @Test
+    public void create_RedeemScriptParser_object_from_erp_multiSig_chunk() {
+        Script redeemScript = RedeemScriptUtils.createErpRedeemScript(
+            defaultFedBtcECKeyList,
+            erpFedBtcECKeyList,
+            500L
+        );
+
+        RedeemScriptParser parser = RedeemScriptParserFactory.get(redeemScript.getChunks());
+
+        Assert.assertEquals(MultiSigType.ERP_FED, parser.getMultiSigType());
+        Assert.assertEquals(ScriptType.REDEEM_SCRIPT, parser.getScriptType());
+    }
+
+    @Test
+    public void create_RedeemScriptParser_object_from_erp_fast_bridge_multiSig_chunk() {
+        Script redeemScript = RedeemScriptUtils.createFastBridgeErpRedeemScript(
+            defaultFedBtcECKeyList,
+            erpFedBtcECKeyList,
+            500L,
+            Sha256Hash.of(new byte[]{1}).getBytes()
+        );
+
+        RedeemScriptParser parser = RedeemScriptParserFactory.get(redeemScript.getChunks());
+
+        Assert.assertEquals(MultiSigType.FAST_BRIDGE_ERP_FED, parser.getMultiSigType());
         Assert.assertEquals(ScriptType.REDEEM_SCRIPT, parser.getScriptType());
     }
 
@@ -52,7 +88,7 @@ public class RedeemScriptParserFactoryTest {
         byte[] data = Sha256Hash.of(new byte[]{1}).getBytes();
         Script fastBridgeRedeemScript = RedeemScriptUtils.createFastBridgeRedeemScript(
             data,
-            btcECKeyList
+            defaultFedBtcECKeyList
         );
 
         Script spk = ScriptBuilder.createP2SHOutputScript(
@@ -69,7 +105,7 @@ public class RedeemScriptParserFactoryTest {
 
     @Test
     public void create_RedeemScriptParser_object_from_standard_P2SH_chunk() {
-        Script redeemScript = RedeemScriptUtils.createStandardRedeemScript(btcECKeyList);
+        Script redeemScript = RedeemScriptUtils.createStandardRedeemScript(defaultFedBtcECKeyList);
 
         Script spk = ScriptBuilder.createP2SHOutputScript(
             2,
@@ -85,7 +121,7 @@ public class RedeemScriptParserFactoryTest {
 
     @Test
     public void create_RedeemScriptParser_object_from_custom_redeem_script_no_multiSig() {
-        Script redeemScript = RedeemScriptUtils.createCustomRedeemScript(btcECKeyList);
+        Script redeemScript = RedeemScriptUtils.createCustomRedeemScript(defaultFedBtcECKeyList);
         RedeemScriptParser parser = RedeemScriptParserFactory.get(redeemScript.getChunks());
 
         Assert.assertEquals(MultiSigType.NO_MULTISIG_TYPE, parser.getMultiSigType());
