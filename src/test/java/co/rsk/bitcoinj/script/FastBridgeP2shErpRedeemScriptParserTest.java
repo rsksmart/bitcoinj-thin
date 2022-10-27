bfcfe6e9b1b2a3080fad414100f96cd1b2e246c7
Joaquin L. Pereyra <joaquinlpereyra@gmail.com>
2022-10-27 18:01:24 -0300

    differential testing for ERP P2SH Fast Bridge

--- src/test/java/co/rsk/bitcoinj/script/FastBridgeP2shErpRedeemScriptParserTest.java
+++ src/test/java/co/rsk/bitcoinj/script/FastBridgeP2shErpRedeemScriptParserTest.java
@@ -1,12 +1,24 @@
 package co.rsk.bitcoinj.script;
 
 import co.rsk.bitcoinj.core.BtcECKey;
+import co.rsk.bitcoinj.core.NetworkParameters;
 import co.rsk.bitcoinj.core.Sha256Hash;
 import co.rsk.bitcoinj.core.VerificationException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.spongycastle.util.encoders.Hex;
 
 public class FastBridgeP2shErpRedeemScriptParserTest {
 
@@ -113,4 +125,44 @@ public class FastBridgeP2shErpRedeemScriptParserTest {
             customRedeemScript.getChunks())
         );
     }
+
+    @Test
+    public void getErpRedeemScript_compareOtherImplementation_P2SHERPFederation() throws IOException {
+
+        byte[] rawRedeemScripts;
+        try {
+            rawRedeemScripts = Files.readAllBytes(Paths.get("src/test/resources/redeemScripts_fastbridge_p2shERP.json"));
+        } catch (IOException e) {
+            System.out.println("redeemScripts_p2shERP.json file not found");
+            throw(e);
+        }
+
+        RawGeneratedRedeemScript[] generatedScripts = new ObjectMapper().readValue(rawRedeemScripts, RawGeneratedRedeemScript[].class);
+        for (RawGeneratedRedeemScript generatedScript : generatedScripts) {
+            Script bitcoinjScript = FastBridgeP2shErpRedeemScriptParser.createFastBridgeP2shErpRedeemScript(
+                    generatedScript.powpegScript,
+                    generatedScript.derivationHash
+
+            );
+
+            Script alternativeScript = generatedScript.script;
+            Assert.assertEquals(alternativeScript, bitcoinjScript);
+        }
+    }
+
+    private static class RawGeneratedRedeemScript {
+        Script powpegScript;
+        Sha256Hash derivationHash;
+        Script script;
+
+        @JsonCreator
+        public RawGeneratedRedeemScript(@JsonProperty("powpegScript") String powpegScript,
+                                        @JsonProperty("derivationHash") String derivationHash,
+                                        @JsonProperty("script") String script) {
+            this.powpegScript = new Script(Hex.decode(powpegScript));
+            this.derivationHash = Sha256Hash.wrap(Hex.decode(derivationHash));
+            this.script = new Script(Hex.decode(script));
+        }
+    }
+
 }
