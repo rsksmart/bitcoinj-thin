1d1530381258125adcdf32cd680b1a44571460fa
Marcos Irisarri <53787863+marcos-iov@users.noreply.github.com>
2023-08-03 18:03:01 -0300

    Update src/main/java/co/rsk/bitcoinj/core/TransactionWitness.java

--- src/main/java/co/rsk/bitcoinj/core/TransactionWitness.java
+++ src/main/java/co/rsk/bitcoinj/core/TransactionWitness.java
@@ -74,7 +74,7 @@ public class TransactionWitness {
         for (TransactionSignature signature : signatures) {
             pushes.add(signature.encodeToBitcoin());
         }
-        pushes.add(new byte[] {});
+        pushes.add(new byte[] {}); // OP_NOTIF argument. If a 0 is set it will validate against the standard keys, if a 1 is set it will validate against the emergency keys
         pushes.add(witnessScript.getProgram());
         return TransactionWitness.of(pushes);
     }
