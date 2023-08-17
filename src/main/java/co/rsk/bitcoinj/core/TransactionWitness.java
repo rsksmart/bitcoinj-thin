9ca9a86dbb3a00a62019834f3221ef6d4c377332
julia zack <julia.zack@iovlabs.org>
2023-08-17 16:33:31 -0300

    Create erpNewRedeemScript and corresponding witness signing

--- src/main/java/co/rsk/bitcoinj/core/TransactionWitness.java
+++ src/main/java/co/rsk/bitcoinj/core/TransactionWitness.java
@@ -68,7 +68,6 @@ public class TransactionWitness {
         return TransactionWitness.of(pushes);
     }
 
-
     public static TransactionWitness createWitnessScriptWithNewRedeem(Script witnessScript, List<TransactionSignature> thresholdSignatures, int signaturesSize) {
         int zeroSignaturesSize = signaturesSize - thresholdSignatures.size();
         List<byte[]> pushes = new ArrayList<>(signaturesSize + 1);
@@ -78,6 +77,34 @@ public class TransactionWitness {
         for (int i = 0; i < zeroSignaturesSize; i ++) {
             pushes.add(new byte[0]);
         }
+        pushes.add(witnessScript.getProgram());
+        return TransactionWitness.of(pushes);
+    }
+
+    public static TransactionWitness createWitnessErpScriptWithNewRedeemStandard(Script witnessScript, List<TransactionSignature> thresholdSignatures, int signaturesSize) {
+        int zeroSignaturesSize = signaturesSize - thresholdSignatures.size();
+        List<byte[]> pushes = new ArrayList<>(signaturesSize + 2);
+        for (int i = 0; i < thresholdSignatures.size(); i++) {
+            pushes.add(thresholdSignatures.get(i).encodeToBitcoin());
+        }
+        for (int i = 0; i < zeroSignaturesSize; i ++) {
+            pushes.add(new byte[0]);
+        }
+        pushes.add(new byte[] {}); // OP_NOTIF argument
+        pushes.add(witnessScript.getProgram());
+        return TransactionWitness.of(pushes);
+    }
+
+    public static TransactionWitness createWitnessErpScriptWithNewRedeemEmergency(Script witnessScript, List<TransactionSignature> thresholdSignatures, int signaturesSize) {
+        int zeroSignaturesSize = signaturesSize - thresholdSignatures.size();
+        List<byte[]> pushes = new ArrayList<>(signaturesSize + 2);
+        for (int i = 0; i < thresholdSignatures.size(); i++) {
+            pushes.add(thresholdSignatures.get(i).encodeToBitcoin());
+        }
+        for (int i = 0; i < zeroSignaturesSize; i ++) {
+            pushes.add(new byte[0]);
+        }
+        pushes.add(new byte[] {1}); // OP_NOTIF argument
         pushes.add(witnessScript.getProgram());
         return TransactionWitness.of(pushes);
     }
