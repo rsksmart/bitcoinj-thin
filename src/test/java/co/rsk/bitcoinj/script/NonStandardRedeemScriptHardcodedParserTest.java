b580557cc1fa1204d41a91f63734a4ea3fc6f251
julia zack <julia.zack@iovlabs.org>
2023-11-24 16:17:08 -0300

    Refactor and split tests from NonStandardRedeemScriptHardcodedParser

--- src/test/java/co/rsk/bitcoinj/script/NonStandardRedeemScriptHardcodedParserTest.java
+++ src/test/java/co/rsk/bitcoinj/script/NonStandardRedeemScriptHardcodedParserTest.java
@@ -3,20 +3,14 @@ package co.rsk.bitcoinj.script;
 import co.rsk.bitcoinj.core.BtcECKey;
 import co.rsk.bitcoinj.core.ScriptException;
 import co.rsk.bitcoinj.core.Sha256Hash;
-import co.rsk.bitcoinj.core.Utils;
 import co.rsk.bitcoinj.script.RedeemScriptParser.MultiSigType;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigInteger;
-import java.util.List;
 
 public class NonStandardRedeemScriptHardcodedParserTest {
-    private final byte[] ERP_TESTNET_REDEEM_SCRIPT_BYTES = Utils.HEX.decode("6453210208f40073a9e43b3e9103acec79767a6de9b0409749884e989960fee578012fce210225e892391625854128c5c4ea4340de0c2a70570f33db53426fc9c746597a03f42102afc230c2d355b1a577682b07bc2646041b5d0177af0f98395a46018da699b6da210344a3c38cd59afcba3edcebe143e025574594b001700dec41e59409bdbd0f2a0921039a060badbeb24bee49eb2063f616c0f0f0765d4ca646b20a88ce828f259fcdb955670300cd50b27552210216c23b2ea8e4f11c3f9e22711addb1d16a93964796913830856b568cc3ea21d3210275562901dd8faae20de0a4166362a4f82188db77dbed4ca887422ea1ec185f1421034db69f2112f4fb1bb6141bf6e2bd6631f0484d0bd95b16767902c9fe219d4a6f5368ae");
-    private final Script redeemScript = new Script(ERP_TESTNET_REDEEM_SCRIPT_BYTES);
-    private final List<ScriptChunk> chunks = redeemScript.getChunks();
-    private final RedeemScriptParser parser = RedeemScriptParserFactory.get(chunks);
-    private final MultiSigType multiSigType = parser.getMultiSigType();
+    private final RedeemScriptParser parser = new NonStandardRedeemScriptHardcodedParser();
 
     @Test
     public void parser_is_NonStandardRedeemScriptHardcodedParser() {
@@ -25,6 +19,7 @@ public class NonStandardRedeemScriptHardcodedParserTest {
 
     @Test
     public void getMultiSigType_returns_expected_value() {
+        MultiSigType multiSigType = parser.getMultiSigType();
         Assert.assertEquals(MultiSigType.NO_MULTISIG_TYPE, multiSigType);
     }
 
