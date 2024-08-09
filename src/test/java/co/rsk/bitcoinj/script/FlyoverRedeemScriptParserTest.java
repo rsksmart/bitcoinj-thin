115b9f52e09b0cb0b4ba3632791590e1ed273566
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-08-09 17:04:26 -0400

    Using publicKeys as a local variable in setUp method

--- src/test/java/co/rsk/bitcoinj/script/FlyoverRedeemScriptParserTest.java
+++ src/test/java/co/rsk/bitcoinj/script/FlyoverRedeemScriptParserTest.java
@@ -10,14 +10,13 @@ import org.junit.Test;
 
 public class FlyoverRedeemScriptParserTest {
 
-    private List<BtcECKey> publicKeys = RedeemScriptTestUtils.getDefaultRedeemScriptKeys();
     private Script redeemScript;
     private byte[] derivationHash;
     private FlyoverRedeemScriptParser flyoverRedeemScriptParser;
 
     @Before
     public void setUp() {
-        publicKeys = RedeemScriptTestUtils.getDefaultRedeemScriptKeys();
+        List<BtcECKey> publicKeys = RedeemScriptTestUtils.getDefaultRedeemScriptKeys();
         redeemScript = ScriptBuilder.createRedeemScript(publicKeys.size() / 2 + 1, publicKeys);
         derivationHash = Sha256Hash.of(new byte[]{1}).getBytes();
         Script flyoverRedeemScript = FlyoverRedeemScriptParser.createFlyoverRedeemScript(
