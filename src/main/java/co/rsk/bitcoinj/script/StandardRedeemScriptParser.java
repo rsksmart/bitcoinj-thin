7f44a27d5d2c8ccd4db25c33f89f72ed86b17ed3
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-08-19 11:30:34 -0400

    Fix adding imports

--- src/main/java/co/rsk/bitcoinj/script/StandardRedeemScriptParser.java
+++ src/main/java/co/rsk/bitcoinj/script/StandardRedeemScriptParser.java
@@ -6,10 +6,12 @@ import co.rsk.bitcoinj.core.BtcECKey;
 import co.rsk.bitcoinj.core.Sha256Hash;
 import co.rsk.bitcoinj.core.Utils;
 import co.rsk.bitcoinj.crypto.TransactionSignature;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class StandardRedeemScriptParser implements RedeemScriptParser {
