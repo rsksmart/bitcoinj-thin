ffeae0c4f5c06380d556478b85548cba7ed9ca1d
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-08-28 16:32:11 -0400

    Using Composite Design Pattern

--- src/main/java/co/rsk/bitcoinj/script/FlyoverRedeemScriptParser.java
+++ src/main/java/co/rsk/bitcoinj/script/FlyoverRedeemScriptParser.java
@@ -1,15 +1,9 @@
 package co.rsk.bitcoinj.script;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
 import co.rsk.bitcoinj.core.BtcECKey;
 import co.rsk.bitcoinj.core.Sha256Hash;
-import co.rsk.bitcoinj.core.Utils;
 import co.rsk.bitcoinj.core.VerificationException;
-import co.rsk.bitcoinj.crypto.TransactionSignature;
-import com.google.common.collect.Lists;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,14 +11,12 @@ import org.slf4j.LoggerFactory;
 public class FlyoverRedeemScriptParser implements RedeemScriptParser {
 
     private static final Logger logger = LoggerFactory.getLogger(FlyoverRedeemScriptParser.class);
-
     private final MultiSigType multiSigType;
-    private final List<ScriptChunk> redeemScriptChunks;
     private final RedeemScriptParser redeemScriptParser;
 
     public FlyoverRedeemScriptParser(List<ScriptChunk> redeemScriptChunks) {
-        this.redeemScriptChunks = extractStandardRedeemScriptChunks(redeemScriptChunks);
-        this.redeemScriptParser = RedeemScriptParserFactory.get(this.redeemScriptChunks.subList(2, this.redeemScriptChunks.size()));
+        List<ScriptChunk> standardRedeemScriptChunks = extractStandardRedeemScriptChunks(redeemScriptChunks);
+        this.redeemScriptParser = RedeemScriptParserFactory.get(standardRedeemScriptChunks);
         this.multiSigType = MultiSigType.FLYOVER;
     }
 
@@ -45,47 +37,22 @@ public class FlyoverRedeemScriptParser implements RedeemScriptParser {
 
     @Override
     public int getM() {
-        checkArgument(redeemScriptChunks.get(0).isOpCode());
-        return Script.decodeFromOpN(redeemScriptChunks.get(0).opcode);
+        return redeemScriptParser.getM();
     }
 
     @Override
     public int findKeyInRedeem(BtcECKey key) {
-        checkArgument(redeemScriptChunks.get(0).isOpCode()); // P2SH scriptSig
-        int numKeys = Script.decodeFromOpN(redeemScriptChunks.get(redeemScriptChunks.size() - 2).opcode);
-        for (int i = 0; i < numKeys; i++) {
-            if (Arrays.equals(redeemScriptChunks.get(1 + i).data, key.getPubKey())) {
-                return i;
-            }
-        }
-
-        throw new IllegalStateException("Could not find matching key " + key.toString() + " in script " + this);
+        return redeemScriptParser.findKeyInRedeem(key);
     }
 
     @Override
     public List<BtcECKey> getPubKeys() {
-        ArrayList<BtcECKey> result = Lists.newArrayList();
-        int numKeys = Script.decodeFromOpN(redeemScriptChunks.get(redeemScriptChunks.size() - 2).opcode);
-        for (int i = 0; i < numKeys; i++) {
-            result.add(BtcECKey.fromPublicOnly(redeemScriptChunks.get(1 + i).data));
-        }
-
-        return result;
+        return redeemScriptParser.getPubKeys();
     }
 
     @Override
     public int findSigInRedeem(byte[] signatureBytes, Sha256Hash hash) {
-        checkArgument(redeemScriptChunks.get(0).isOpCode()); // P2SH scriptSig
-        int numKeys = Script.decodeFromOpN(redeemScriptChunks.get(redeemScriptChunks.size() - 2).opcode);
-        TransactionSignature signature = TransactionSignature.decodeFromBitcoin(signatureBytes, true);
-        for (int i = 0; i < numKeys; i++) {
-            if (BtcECKey.fromPublicOnly(redeemScriptChunks.get(i + 1).data).verify(hash, signature)) {
-                return i;
-            }
-        }
-        throw new IllegalStateException("Could not find matching key for signature on " + hash.toString()
-            + " sig " + Utils.HEX.encode(signatureBytes)
-        );
+        return redeemScriptParser.findSigInRedeem(signatureBytes, hash);
     }
 
     @Override
