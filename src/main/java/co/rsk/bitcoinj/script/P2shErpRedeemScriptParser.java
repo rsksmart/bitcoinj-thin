bd664a4af4c689a47aec935c96a08e5f301245d3
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-09-08 19:17:45 -0400

    Remove createP2shErpRedeemScript  method from P2shErpFederationRedeemScriptParser
    At this time we need to remove methods that RedeemScriptUtils already covered so we will remove and refactor tests and look at RSKj to see the impact this has on it.
    
    - Remove createP2shErpRedeemScript  method from P2shErpFederationRedeemScriptParser
    
    - Remove tests related to createP2shErpRedeemScript.
    
    - Ensure all tests are running.
    
    Important Note: All the tests removed will be analyzed and moved to a new class called RedeemScriptUtilsTest.java, to cover the createP2shErpRedeemScript method with tests.

--- src/main/java/co/rsk/bitcoinj/script/P2shErpRedeemScriptParser.java
+++ src/main/java/co/rsk/bitcoinj/script/P2shErpRedeemScriptParser.java
@@ -74,75 +74,7 @@ public class P2shErpRedeemScriptParser implements RedeemScriptParser {
         return chunksForRedeem;
     }
 
-    public static Script createP2shErpRedeemScript(
-        Script defaultFederationRedeemScript,
-        Script erpFederationRedeemScript,
-        Long csvValue
-    ) {
-        byte[] serializedCsvValue = Utils.signedLongToByteArrayLE(csvValue);
-
-        validateP2shErpRedeemScriptValues(
-            defaultFederationRedeemScript,
-            erpFederationRedeemScript,
-            csvValue
-        );
-
-        ScriptBuilder scriptBuilder = new ScriptBuilder();
-
-        Script erpRedeemScript = scriptBuilder
-            .op(ScriptOpCodes.OP_NOTIF)
-            .addChunks(defaultFederationRedeemScript.getChunks())
-            .op(ScriptOpCodes.OP_ELSE)
-            .data(serializedCsvValue)
-            .op(ScriptOpCodes.OP_CHECKSEQUENCEVERIFY)
-            .op(ScriptOpCodes.OP_DROP)
-            .addChunks(erpFederationRedeemScript.getChunks())
-            .op(ScriptOpCodes.OP_ENDIF)
-            .build();
-
-        // Validate the created redeem script has a valid structure
-        if (!RedeemScriptValidator.hasP2shErpRedeemScriptStructure(erpRedeemScript.getChunks())) {
-            String message = String.format(
-                "Created redeem script has an invalid structure, not P2SH ERP redeem script. Redeem script created: %s",
-                erpRedeemScript
-            );
-            logger.debug("[createErpRedeemScript] {}", message);
-            throw new VerificationException(message);
-        }
-
-        return erpRedeemScript;
-    }
-
     public static boolean isP2shErpFed(List<ScriptChunk> chunks) {
         return RedeemScriptValidator.hasP2shErpRedeemScriptStructure(chunks);
-    }
-
-    private static void validateP2shErpRedeemScriptValues(
-        Script defaultFederationRedeemScript,
-        Script erpFederationRedeemScript,
-        Long csvValue
-    ) {
-        if (!RedeemScriptValidator.hasStandardRedeemScriptStructure(defaultFederationRedeemScript.getChunks()) ||
-            !RedeemScriptValidator.hasStandardRedeemScriptStructure(erpFederationRedeemScript.getChunks())) {
-
-            String message = "Provided redeem scripts has an invalid structure, not standard";
-            logger.debug(
-                "[validateP2shErpRedeemScriptValues] {}. Default script {}. Emergency script {}",
-                message,
-                defaultFederationRedeemScript,
-                erpFederationRedeemScript
-            );
-            throw new VerificationException(message);
-        }
-
-        if (csvValue <= 0 || csvValue > MAX_CSV_VALUE) {
-            String message = String.format(
-                "Provided csv value %d must be between 0 and %d",
-                csvValue,
-                MAX_CSV_VALUE
-            );
-            logger.warn("[validateP2shErpRedeemScriptValues] {}", message);
-            throw new VerificationException(message);
-        }
     }
 }
