9ee480bc7baddc7ef2608a5a89451ad749b2df18
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-09-12 05:40:44 -0400

    Refactor FlyoverRedeemScriptParserTest
    - Remove the "For" to iterate different kind of Scripts since this is hard to follow.
    
    - Split the test as per the script type and method.

--- src/test/java/co/rsk/bitcoinj/script/FlyoverRedeemScriptParserTest.java
+++ src/test/java/co/rsk/bitcoinj/script/FlyoverRedeemScriptParserTest.java
@@ -16,7 +16,6 @@ import co.rsk.bitcoinj.crypto.TransactionSignature;
 import co.rsk.bitcoinj.params.MainNetParams;
 import co.rsk.bitcoinj.script.RedeemScriptParser.MultiSigType;
 import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,8 +31,6 @@ public class FlyoverRedeemScriptParserTest {
     private Script flyoverErpRedeemScript;
     private Script p2shErpRedeemScript;
     private Script flyoverP2shErpRedeemScript;
-    private List<Script> malformedScripts;
-    private List<Script> flyoverRedeemScripts;
 
     @Before
     public void setUp() {
@@ -46,96 +43,159 @@ public class FlyoverRedeemScriptParserTest {
 
         p2shErpRedeemScript = RedeemScriptUtils.createP2shErpRedeemScript(defaultRedeemScriptKeys, emergencyRedeemScriptKeys, CSV_VALUE);
         flyoverP2shErpRedeemScript = RedeemScriptUtils.createFlyoverRedeemScript(derivationArgumentsHash.getBytes(), p2shErpRedeemScript);
-
-        flyoverRedeemScripts = Arrays.asList(flyoverStandardRedeemScript, flyoverErpRedeemScript, flyoverP2shErpRedeemScript);
-
-        Script malformedScriptZeroSize = new Script(new byte[0]);
-        Script malformedScriptTwoSize = new Script(new byte[2]);
-
-        malformedScripts = Arrays.asList(malformedScriptZeroSize, malformedScriptTwoSize);
     }
 
     @Test
-    public void getMultiSigType_whenIsValidRedeemScript_shouldReturnFlyoverMultiSigType() {
-        for(Script flyoverRedeemScript : flyoverRedeemScripts) {
-            // Arrange
-            FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
-
-            // Act
-            MultiSigType actualMultiSigType = flyoverRedeemScriptParser.getMultiSigType();
-
-            // Assert
-            assertEquals(MultiSigType.FLYOVER, actualMultiSigType);
-        }
+    public void getMultiSigType_whenIsStandardRedeemScript_shouldReturnFlyoverMultiSigType() {
+        assertIsFlyoverMultiSigType(flyoverStandardRedeemScript);
     }
 
     @Test
-    public void getM_whenFlyoverRedeemScriptContainsValidRedeemScript_shouldReturnMValue() {
+    public void getMultiSigType_whenIsErpRedeemScript_shouldReturnFlyoverMultiSigType() {
+        assertIsFlyoverMultiSigType(flyoverErpRedeemScript);
+    }
+
+    @Test
+    public void getMultiSigType_whenIsP2shErpRedeemScript_shouldReturnFlyoverMultiSigType() {
+        assertIsFlyoverMultiSigType(flyoverP2shErpRedeemScript);
+    }
+
+    private void assertIsFlyoverMultiSigType(Script flyoverRedeemScript) {
+        // Arrange
+        FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
+
+        // Act
+        MultiSigType actualMultiSigType = flyoverRedeemScriptParser.getMultiSigType();
+
+        // Assert
+        assertEquals(MultiSigType.FLYOVER, actualMultiSigType);
+    }
+
+    @Test
+    public void getM_whenFlyoverRedeemScriptContainsStandardRedeemScript_shouldReturnMValue() {
+        assertGetMValue(flyoverStandardRedeemScript);
+    }
+
+    @Test
+    public void getM_whenFlyoverRedeemScriptContainsErpRedeemScript_shouldReturnMValue() {
+        assertGetMValue(flyoverErpRedeemScript);
+    }
+
+    @Test
+    public void getM_whenFlyoverRedeemScriptContainsP2shErpRedeemScript_shouldReturnMValue() {
+        assertGetMValue(flyoverP2shErpRedeemScript);
+    }
+
+    private void assertGetMValue(Script flyoverRedeemScript) {
         // Arrange
         final int EXPECTED_M = 5;
-        for (Script flyoverRedeemScript : flyoverRedeemScripts) {
-            FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
+        FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
 
-            // Act
-            int actualM = flyoverRedeemScriptParser.getM();
+        // Act
+        int actualM = flyoverRedeemScriptParser.getM();
 
-            // Assert
-            assertEquals(EXPECTED_M, actualM);
-        }
+        // Assert
+        assertEquals(EXPECTED_M, actualM);
     }
 
     @Test
-    public void findKeyInRedeem_whenKeyIsInRedeemScript_shouldReturnKeyIndexPosition() {
+    public void findKeyInRedeem_whenKeyIsInStandardRedeemScript_shouldReturnKeyIndexPosition() {
+        assertKeyInRedeem(flyoverStandardRedeemScript);
+    }
+
+    @Test
+    public void findKeyInRedeem_whenKeyIsInErpRedeemScript_shouldReturnKeyIndexPosition() {
+        assertKeyInRedeem(flyoverErpRedeemScript);
+    }
+
+    @Test
+    public void findKeyInRedeem_whenKeyIsInP2shErpRedeemScript_shouldReturnKeyIndexPosition() {
+        assertKeyInRedeem(flyoverP2shErpRedeemScript);
+    }
+
+    private void assertKeyInRedeem(Script flyoverRedeemScript) {
         // Arrange
         final int EXPECTED_KEY_INDEX = 5;
-        for (Script flyoverRedeemScript : flyoverRedeemScripts) {
-            FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
+        FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
 
-            // Act
-            int actualKeyIndex = flyoverRedeemScriptParser.findKeyInRedeem(defaultRedeemScriptKeys.get(EXPECTED_KEY_INDEX));
+        // Act
+        int actualKeyIndex = flyoverRedeemScriptParser.findKeyInRedeem(defaultRedeemScriptKeys.get(EXPECTED_KEY_INDEX));
 
-            // Assert
-            assertEquals(EXPECTED_KEY_INDEX, actualKeyIndex);
-        }
+        // Assert
+        assertEquals(EXPECTED_KEY_INDEX, actualKeyIndex);
     }
 
-    @Test
-    public void findKeyInRedeem_whenKeyIsNotInRedeemScript_shouldThrowIllegalStateException() {
+    @Test(expected = IllegalStateException.class)
+    public void findKeyInRedeem_whenKeyIsNotInStandardRedeemScript_shouldThrowIllegalStateException() {
+        assertThrowsIllegalStateException(flyoverStandardRedeemScript);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void findKeyInRedeem_whenKeyIsNotInErpRedeemScript_shouldThrowIllegalStateException() {
+        assertThrowsIllegalStateException(flyoverErpRedeemScript);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void findKeyInRedeem_whenKeyIsNotInP2shErpRedeemScript_shouldThrowIllegalStateException() {
+        assertThrowsIllegalStateException(flyoverP2shErpRedeemScript);
+    }
+
+    private void assertThrowsIllegalStateException(Script flyoverRedeemScript) {
         // Arrange
         final BtcECKey differentKey = BtcECKey.fromPrivate(BigInteger.valueOf(1000));
-        for(Script flyoverRedeemScript : flyoverRedeemScripts) {
-            FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
-            try {
-                // Act
-                flyoverRedeemScriptParser.findKeyInRedeem(differentKey);
-            } catch (IllegalStateException actualException) {
-                // Assert
-                assertEquals(IllegalStateException.class, actualException.getClass());
-            }
-        }
+        FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
+
+        // Act - Assert
+        flyoverRedeemScriptParser.findKeyInRedeem(differentKey);
     }
 
     @Test
-    public void getPubKeys_whenFlyoverRedeemScriptContainsValidRedeemScript_shouldReturnPubKeys() {
-        for (Script flyoverRedeemScript : flyoverRedeemScripts) {
-            // Arrange
-            FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
-
-            // Act
-            List<BtcECKey> actualPubKeys = flyoverRedeemScriptParser.getPubKeys();
-
-            // Assert
-            defaultRedeemScriptKeys.forEach(expectedPubKey -> {
-                int btcECKeyIndex = defaultRedeemScriptKeys.indexOf(expectedPubKey);
-                BtcECKey btcECKey = actualPubKeys.get(btcECKeyIndex);
-                byte[] actualPubKey = btcECKey.getPubKey();
-                assertThat(actualPubKey, equalTo(expectedPubKey.getPubKey()));
-            });
-        }
+    public void getPubKeys_whenFlyoverRedeemScriptContainsStandardRedeemScript_shouldReturnPubKeys() {
+        assertPubKeys(flyoverStandardRedeemScript);
     }
 
     @Test
-    public void findSigInRedeem_whenSignatureIsInValidRedeemScript_shouldReturnSignatureIndexPosition() {
+    public void getPubKeys_whenFlyoverRedeemScriptContainsErpRedeemScript_shouldReturnPubKeys() {
+        assertPubKeys(flyoverErpRedeemScript);
+    }
+
+    @Test
+    public void getPubKeys_whenFlyoverRedeemScriptContainsP2shErpRedeemScript_shouldReturnPubKeys() {
+        assertPubKeys(flyoverP2shErpRedeemScript);
+    }
+
+    private void assertPubKeys(Script flyoverRedeemScript) {
+        // Arrange
+        FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
+
+        // Act
+        List<BtcECKey> actualPubKeys = flyoverRedeemScriptParser.getPubKeys();
+
+        // Assert
+        defaultRedeemScriptKeys.forEach(expectedPubKey -> {
+            int btcECKeyIndex = defaultRedeemScriptKeys.indexOf(expectedPubKey);
+            BtcECKey btcECKey = actualPubKeys.get(btcECKeyIndex);
+            byte[] actualPubKey = btcECKey.getPubKey();
+            assertThat(actualPubKey, equalTo(expectedPubKey.getPubKey()));
+        });
+    }
+
+    @Test
+    public void findSigInRedeem_whenSignatureIsInStandardRedeemScript_shouldReturnSignatureIndexPosition() {
+        assertSigInRedeem(flyoverStandardRedeemScript);
+    }
+
+    @Test
+    public void findSigInRedeem_whenSignatureIsInErpRedeemScript_shouldReturnSignatureIndexPosition() {
+        assertSigInRedeem(flyoverErpRedeemScript);
+    }
+
+    @Test
+    public void findSigInRedeem_whenSignatureIsInP2shErpRedeemScript_shouldReturnSignatureIndexPosition() {
+        assertSigInRedeem(flyoverP2shErpRedeemScript);
+    }
+
+    private void assertSigInRedeem(Script flyoverRedeemScript){
         // Arrange
         final int EXPECTED_SIGNATURE_INDEX = 0;
         final int SIGNATURE_INPUT_INDEX = 0;
@@ -164,15 +224,13 @@ public class FlyoverRedeemScriptParserTest {
         ECDSASignature signature = privateKey.sign(hashForSignatureHash);
         TransactionSignature transactionSignature = new TransactionSignature(signature, BtcTransaction.SigHash.ALL, false);
 
-        for (Script flyoverRedeemScript : flyoverRedeemScripts) {
-            FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
+        FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
 
-            // Act
-            int actualSignatureIndex = flyoverRedeemScriptParser.findSigInRedeem(transactionSignature.encodeToBitcoin(), hashForSignatureHash);
+        // Act
+        int actualSignatureIndex = flyoverRedeemScriptParser.findSigInRedeem(transactionSignature.encodeToBitcoin(), hashForSignatureHash);
 
-            // Assert
-            assertEquals(EXPECTED_SIGNATURE_INDEX, actualSignatureIndex);
-        }
+        // Assert
+        assertEquals(EXPECTED_SIGNATURE_INDEX, actualSignatureIndex);
     }
 
     @Test
@@ -214,16 +272,19 @@ public class FlyoverRedeemScriptParserTest {
         assertEquals(expectedRedeemScriptChunks, actualRedeemScriptChunks);
     }
 
-    @Test
-    public void flyoverRedeemScriptParser_whenRedeemScriptChunksIsMalformed_shouldThrowVerificationException(){
-        for (Script script : malformedScripts) {
-            try {
-                // Act
-                new FlyoverRedeemScriptParser(script.getChunks());
-            } catch (VerificationException actualException) {
-                // Assert
-                assertEquals(VerificationException.class, actualException.getClass());
-            }
-        }
+    @Test(expected = VerificationException.class)
+    public void flyoverRedeemScriptParser_whenRedeemScriptChunksSizeIsZero_shouldThrowVerificationException(){
+        Script malformedScriptZeroSize = new Script(new byte[0]);
+
+        // Act
+        new FlyoverRedeemScriptParser(malformedScriptZeroSize.getChunks());
+    }
+
+    @Test(expected = VerificationException.class)
+    public void flyoverRedeemScriptParser_whenRedeemScriptChunksSizeIsTwo_shouldThrowVerificationException(){
+        Script malformedScriptTwoSize = new Script(new byte[2]);
+
+        // Act
+        new FlyoverRedeemScriptParser(malformedScriptTwoSize.getChunks());
     }
 }
