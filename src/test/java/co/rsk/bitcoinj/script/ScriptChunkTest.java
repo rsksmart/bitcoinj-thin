b18cd018ddfe5df3fe24ebadbb19d9c0d401621d
Marcos <marcos@rootstocklabs.com>
2025-05-30 12:13:49 -0300

    Move isN() and isOpCheckMultiSig() to ScriptChunk class

--- src/test/java/co/rsk/bitcoinj/script/ScriptChunkTest.java
+++ src/test/java/co/rsk/bitcoinj/script/ScriptChunkTest.java
@@ -46,4 +46,142 @@ public class ScriptChunkTest {
         assertFalse("push of 255 bytes", new ScriptChunk(OP_PUSHDATA2, new byte[255]).isShortestPossiblePushData());
         assertFalse("push of 65535 bytes", new ScriptChunk(OP_PUSHDATA4, new byte[65535]).isShortestPossiblePushData());
     }
+
+    @Test
+    public void isOpCheckMultiSig_withNullData_returnsTrue() {
+        ScriptChunk chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKMULTISIG, null);
+        assertTrue(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKMULTISIGVERIFY, null);
+        assertTrue(chunk.isOpCheckMultiSig());
+    }
+
+    @Test
+    public void isOpCheckMultiSig_withEmptyData_returnsTrue() {
+        byte[] emptyData = new byte[]{};
+
+        ScriptChunk chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKMULTISIG, emptyData);
+        assertTrue(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKMULTISIGVERIFY, emptyData);
+        assertTrue(chunk.isOpCheckMultiSig());
+    }
+
+    @Test
+    public void isOpCheckMultiSig_withData_returnsTrue() {
+        byte[] randomData = new byte[]{1, 2, 3};
+
+        ScriptChunk chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKMULTISIG, randomData);
+        assertTrue(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKMULTISIGVERIFY, randomData);
+        assertTrue(chunk.isOpCheckMultiSig());
+    }
+
+    @Test
+    public void isOpCheckMultiSig_withNonOpCheckMultiSig_nullData_returnsFalse() {
+        ScriptChunk chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKSIG, null);
+        assertFalse(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_RETURN, null);
+        assertFalse(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_0, null);
+        assertFalse(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_1, null);
+        assertFalse(chunk.isOpCheckMultiSig());
+    }
+
+    @Test
+    public void isOpCheckMultiSig_withNonOpCheckMultiSig_emptyData_returnsFalse() {
+        byte[] emptyData = new byte[]{};
+
+        ScriptChunk chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKSIG, emptyData);
+        assertFalse(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_RETURN, emptyData);
+        assertFalse(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_0, emptyData);
+        assertFalse(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_1, emptyData);
+        assertFalse(chunk.isOpCheckMultiSig());
+    }
+
+    @Test
+    public void isOpCheckMultiSig_withNonOpCheckMultiSig_withData_returnsFalse() {
+        byte[] randomData = new byte[]{1, 2, 3};
+
+        ScriptChunk chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKSIG, randomData);
+        assertFalse(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_RETURN, randomData);
+        assertFalse(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_0, randomData);
+        assertFalse(chunk.isOpCheckMultiSig());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_1, randomData);
+        assertFalse(chunk.isOpCheckMultiSig());
+    }
+
+    @Test
+    public void isOpCheckMultiSig_withNonOpCode_returnsFalse() {
+        ScriptChunk chunk = new ScriptChunk(OP_PUSHDATA1, null);
+        assertFalse(chunk.isOpCode());
+        assertFalse(chunk.isOpCheckMultiSig());
+    }
+
+    @Test
+    public void isN_withSmallNumber_returnsTrue() {
+        for (int i=0; i<16; i++) {
+            ScriptChunk chunk = new ScriptChunk(ScriptOpCodes.OP_1 + i, null);
+            assertTrue(chunk.isN());
+        }
+    }
+
+    @Test
+    public void isN_withPushData_returnsTrue() {
+        // Need to review the range of values for OP_PUSHDATA1, OP_PUSHDATA2, and OP_PUSHDATA4,
+        // above 127 they are serialized as negative numbers and isPushDataNumber() will return false.
+        for (int i=1; i<100; i++) {
+            ScriptChunk chunk = new ScriptChunk(ScriptOpCodes.OP_PUSHDATA1, new byte[]{(byte) i});
+            assertTrue(chunk.isN());
+
+            chunk = new ScriptChunk(ScriptOpCodes.OP_PUSHDATA2, new byte[]{(byte) i});
+            assertTrue(chunk.isN());
+
+            chunk = new ScriptChunk(ScriptOpCodes.OP_PUSHDATA4, new byte[]{(byte) i});
+            assertTrue(chunk.isN());
+        }
+    }
+
+    @Test
+    public void isN_withNonNumber_returnsFalse() {
+        ScriptChunk chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKMULTISIG, null);
+        assertFalse(chunk.isN());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_CHECKSIG, null);
+        assertFalse(chunk.isN());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_RETURN, null);
+        assertFalse(chunk.isN());
+
+        chunk = new ScriptChunk(ScriptOpCodes.OP_0, null);
+        assertFalse(chunk.isN());
+    }
+
+    @Test
+    public void isN_withNonPushData_returnsFalse() {
+        ScriptChunk chunk = new ScriptChunk(OP_PUSHDATA1, null);
+        assertFalse(chunk.isN());
+
+        chunk = new ScriptChunk(OP_PUSHDATA2, null);
+        assertFalse(chunk.isN());
+
+        chunk = new ScriptChunk(OP_PUSHDATA4, null);
+        assertFalse(chunk.isN());
+    }
 }
