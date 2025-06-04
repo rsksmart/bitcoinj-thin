6695c828bc20f8202fb6bc7df948449150157c67
Julian Len <julian@iovlabs.org>
2025-06-04 17:54:24 -0300

    feat: defaultRedeemScriptKeys now returns 20 keys

--- src/test/java/co/rsk/bitcoinj/script/FlyoverRedeemScriptParserTest.java
+++ src/test/java/co/rsk/bitcoinj/script/FlyoverRedeemScriptParserTest.java
@@ -78,7 +78,7 @@ public class FlyoverRedeemScriptParserTest {
 
     private void assertGetMValue(Script flyoverRedeemScript) {
         // Arrange
-        final int EXPECTED_M = 5;
+        final int EXPECTED_M = 11;
         FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
 
         // Act
@@ -129,7 +129,7 @@ public class FlyoverRedeemScriptParserTest {
 
     private void assertThrowsIllegalStateException(Script flyoverRedeemScript) {
         // Arrange
-        final BtcECKey differentKey = BtcECKey.fromPrivate(BigInteger.valueOf(1000));
+        final BtcECKey differentKey = BtcECKey.fromPrivate(BigInteger.valueOf(10001));
         FlyoverRedeemScriptParser flyoverRedeemScriptParser = new FlyoverRedeemScriptParser(flyoverRedeemScript.getChunks());
 
         // Act - Assert
